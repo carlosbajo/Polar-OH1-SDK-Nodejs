73e8e6820fb9379919194731f475af42ae9ea31c
Carlos Bajo <carlosabpreciado@gmail.com>
2020-03-02 21:34:54 -0700

    Problema del generador

--- lib/DeviceManager.ts
+++ lib/DeviceManager.ts
@@ -3,7 +3,7 @@ import Oh1 from './Oh1';
 export default class DeviceManager {
     constructor() {}
 
-    public DeviceOH1(deviceAddress: string): Oh1 {
+    public DeviceOH1(deviceAddress: string = 'a0-9e-1a-23-ac-d0'): Oh1 {
         return new Oh1(deviceAddress);
     }
 }
